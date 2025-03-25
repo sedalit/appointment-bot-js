8e559697a917d599d642009979795ca5eb712087
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-03-25 20:50:21 +1000

    Add callback for enter test scene

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -1,7 +1,8 @@
-const Parser = require('rss-parser');
 const { InlineKeyboard } = require('../buttons');
 const answers = require('../answers.json');
 
 module.exports.CallbackHandler = (bot) => {
-    
+    bot.action('enterTestScene', async (ctx) => {
+        await ctx.scene.enter('test');
+    });
 };
